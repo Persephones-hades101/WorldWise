c242c1059eb27ab1daf803bcf1c865f81a79aaa5
sudh4800@gmail.com <sudh4800@gmail.com>
2025-04-10 07:08:32 +0530

    fetched cities data from cities json api hosted on reder instead of fetching from json server

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,79 +1,95 @@
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap,
+  useMapEvent,
+} from 'react-leaflet';
 
-import { useNavigate, useSearchParams } from 'react-router-dom'
-import { useEffect, useState } from "react"
-import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvent } from "react-leaflet"
+import styles from './Map.module.css';
 
-import styles from './Map.module.css'
-
-import { useCities } from "../contexts/CitiesContext"
-import { useGeolocation } from '../hooks/useGeoLocation'
-import { useUrlPosition } from '../hooks/useUrlPosition'
-import Button from "./Button"
+import { useCities } from '../contexts/CitiesContext';
+import { useGeolocation } from '../hooks/useGeoLocation';
+import { useUrlPosition } from '../hooks/useUrlPosition';
+import Button from './Button';
 
 export default function Map() {
-  const [mapPosition, setMapPosition] = useState([40, 0])
-  const { cities } = useCities()
-  const { position: geoLocationPosition, isLoading: isLoadingPosition, getPosition } = useGeolocation()
-  const [mapLat, mapLng] = useUrlPosition()
+  const [mapPosition, setMapPosition] = useState([40, 0]);
+  const { cities } = useCities();
+  const {
+    position: geoLocationPosition,
+    isLoading: isLoadingPosition,
+    getPosition,
+  } = useGeolocation();
+  const [mapLat, mapLng] = useUrlPosition();
+  // console.log(cities);
   // console.log(mapLat)
 
+  useEffect(
+    function () {
+      if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+    },
+    [mapLat, mapLng],
+  );
 
-  useEffect(function () {
-    if (mapLat && mapLng)
-      setMapPosition([mapLat, mapLng])
-  }, [mapLat, mapLng])
-
-  useEffect(function () {
-    if (geoLocationPosition)
-      setMapPosition([geoLocationPosition.lat, geoLocationPosition.lng])
-  }, [geoLocationPosition])
-
+  useEffect(
+    function () {
+      if (geoLocationPosition)
+        setMapPosition([geoLocationPosition.lat, geoLocationPosition.lng]);
+    },
+    [geoLocationPosition],
+  );
 
   return (
     <div className={styles.mapContainer}>
-      {!geoLocationPosition && <Button type='position' onclick={getPosition}>
-        {isLoadingPosition ? "Loading..." : "Use your location"}
-      </Button>}
+      {!geoLocationPosition && (
+        <Button type="position" onclick={getPosition}>
+          {isLoadingPosition ? 'Loading...' : 'Use your location'}
+        </Button>
+      )}
       <MapContainer
         className={styles.map}
         center={mapPosition}
         zoom={6}
-        scrollWheelZoom={true}>
+        scrollWheelZoom={true}
+      >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        {
-          cities.map((city) => (
-            <Marker position={[city.position.lat, city.position.lng]} key={city.id}>
-              <Popup>
-                {city.emoji} {city.cityName}
-              </Popup>
-            </Marker>
-          ))
-        }
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              {city.emoji} {city.cityName}
+            </Popup>
+          </Marker>
+        ))}
         <ChangeCenter position={mapPosition} />
         <DetectClick setMapPosition={setMapPosition} />
       </MapContainer>
-    </div >
-  )
+    </div>
+  );
 }
 
-
-
 function ChangeCenter({ position }) {
-  const map = useMap()
-  map.setView(position)
+  const map = useMap();
+  map.setView(position);
   return null;
 }
 
 function DetectClick({ setMapPosition }) {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   useMapEvent({
     click: (e) => {
-      setMapPosition([e.latlng.lat, e.latlng.lng])
-      navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`)
-    }
-  })
+      setMapPosition([e.latlng.lat, e.latlng.lng]);
+      navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`);
+    },
+  });
 }
--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,141 +1,171 @@
-import { createContext, useCallback, useContext, useEffect, useReducer, useState } from "react";
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useReducer,
+  useState,
+} from 'react';
 
-const CitiesContext = createContext()
+const CitiesContext = createContext();
 
-const BASE_URL = "http://localhost:9000"
+const BASE_URL = 'https://cities-api-for-worldwise.onrender.com';
 
 const initialState = {
   cities: [],
   isLoading: false,
   currentCity: {},
-  error: ""
-}
+  error: '',
+};
 
 function reducer(state, action) {
   switch (action.type) {
-    case "loading":
-      return { ...state, isLoading: true }
-    case "cities/loaded":
+    case 'loading':
+      return { ...state, isLoading: true };
+    case 'cities/loaded':
       return {
-        ...state, isLoading: false, cities: action.payload
-      }
+        ...state,
+        isLoading: false,
+        cities: action.payload,
+      };
 
+    case 'city/loaded':
+      return {
+        ...state,
+        isLoading: false,
+        currentCity: action.payload,
+      };
 
-    case "city/loaded":
+    case 'city/created':
       return {
-        ...state, isLoading: false, currentCity: action.payload
-      }
+        ...state,
+        isLoading: false,
+        cities: [...state.cities, action.payload],
+        currentCity: action.payload,
+      };
+    case 'city/deleted':
+      return {
+        ...state,
+        isLoading: false,
+        cities: state.cities.filter((city) => city.id !== action.payload),
+        currentCity: {},
+      };
 
-    case "city/created":
+    case 'rejected':
       return {
-        ...state, isLoading: false, cities: [...state.cities, action.payload], currentCity: action.payload
-      }
-    case "city/deleted":
-      return {
-        ...state, isLoading: false, cities: state.cities.filter(city => city.id !== action.payload), currentCity: {}
-      }
-
-    case "rejected":
-      return {
-        ...state, isLoading: false, error: action.payload
-      }
+        ...state,
+        isLoading: false,
+        error: action.payload,
+      };
     default:
-      throw new Error("Unknown action!")
+      throw new Error('Unknown action!');
   }
 }
 
-
 function CitiesProvider({ children }) {
-
   // const [cities, setCities] = useState([])
   // const [isLoading, setIsLoading] = useState(false)
   // const [currentCity, setCurrentCity] = useState({})
 
-  const [{ cities, isLoading, currentCity, error }, dispatch] = useReducer(reducer, initialState)
-
+  const [{ cities, isLoading, currentCity, error }, dispatch] = useReducer(
+    reducer,
+    initialState,
+  );
 
   useEffect(function () {
     async function fetchCities() {
       try {
-        dispatch({ type: "loading" })
-        const res = await fetch(`${BASE_URL}/cities`)
-        const data = await res.json()
-        dispatch({ type: "cities/loaded", payload: data })
+        dispatch({ type: 'loading' });
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        dispatch({ type: 'cities/loaded', payload: data });
       } catch (error) {
-        dispatch({ type: "rejected", payload: "Something went wrong while fetching the cities" })
+        dispatch({
+          type: 'rejected',
+          payload: 'Something went wrong while fetching the cities',
+        });
       }
-
     }
-    fetchCities()
-  }, [])
+    fetchCities();
+  }, []);
 
-
-  const getCity = useCallback(async function getCity(id) {
-    if (Number(id) === currentCity.id) return;
-    try {
-      dispatch({ type: "loading" })
-      const res = await fetch(`${BASE_URL}/cities/${id}`)
-      const data = await res.json()
-      // console.log(data)
-      dispatch({ type: "city/loaded", payload: data })
-    } catch (error) {
-      dispatch({ type: "rejected", payload: "Something went wrong while fetching the city" })
-    }
-  }, [currentCity.id])
+  const getCity = useCallback(
+    async function getCity(id) {
+      if (Number(id) === currentCity.id) return;
+      try {
+        dispatch({ type: 'loading' });
+        const res = await fetch(`${BASE_URL}/cities/${id}`);
+        const data = await res.json();
+        // console.log(data)
+        dispatch({ type: 'city/loaded', payload: data });
+      } catch (error) {
+        dispatch({
+          type: 'rejected',
+          payload: 'Something went wrong while fetching the city',
+        });
+      }
+    },
+    [currentCity.id],
+  );
 
   async function createCity(newCity) {
+    console.log(newCity);
     try {
-      dispatch({ type: "loading" })
+      dispatch({ type: 'loading' });
       const res = await fetch(`${BASE_URL}/cities`, {
-        method: "POST",
+        method: 'POST',
         body: JSON.stringify(newCity),
         headers: {
-          "Content-Type": "application/json"
-        }
-      })
-      const data = await res.json()
-      dispatch({ type: "city/created", payload: data })
+          'Content-Type': 'application/json',
+        },
+      });
+      const data = await res.json();
+      dispatch({ type: 'city/created', payload: data });
     } catch (error) {
-      dispatch({ type: "rejected", payload: "Something went wrong while creating the cities" })
+      dispatch({
+        type: 'rejected',
+        payload: 'Something went wrong while creating the cities',
+      });
     }
   }
 
   async function deleteCity(id) {
     try {
-      dispatch({ type: "loading" })
+      dispatch({ type: 'loading' });
       await fetch(`${BASE_URL}/cities/${id}`, {
-        method: "DELETE"
-      }
-      )
-      dispatch({ type: "city/deleted", payload: id })
+        method: 'DELETE',
+      });
+      dispatch({ type: 'city/deleted', payload: id });
     } catch (error) {
-      dispatch({ type: "rejected", payload: "Something went wrong while deleting the cities" })
+      dispatch({
+        type: 'rejected',
+        payload: 'Something went wrong while deleting the cities',
+      });
     }
   }
 
-
-
-
   return (
-    <CitiesContext.Provider value={{
-      cities,
-      isLoading,
-      currentCity,
-      error,
-      getCity,
-      createCity,
-      deleteCity
-    }}>
+    <CitiesContext.Provider
+      value={{
+        cities,
+        isLoading,
+        currentCity,
+        error,
+        getCity,
+        createCity,
+        deleteCity,
+      }}
+    >
       {children}
     </CitiesContext.Provider>
-  )
+  );
 }
 
 function useCities() {
-  const context = useContext(CitiesContext)
-  if (context === undefined) throw new Error("CitiesContext was used outside the CitiesProvider")
-  return context
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error('CitiesContext was used outside the CitiesProvider');
+  return context;
 }
-
 
 export { CitiesProvider, useCities };
